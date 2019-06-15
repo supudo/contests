885398d177c56a6f80ee48169cd2ff37d171235b
Sergey Petrov <supudo@gmail.com>
2019-06-15 21:50:17 +0300

    rev

--- reverse_array.cpp
+++ reverse_array.cpp
@@ -23,14 +23,29 @@ int main() {
   }
   cout << endl;
 
-  int arrrev[arrsize];
-  for (int i=arrsize-1, j=0; i>=0; i--, j++) {
-    arrrev[j] = arr[i];
+  // int arrrev[arrsize];
+  // for (int i=arrsize-1, j=0; i>=0; i--, j++) {
+  //   arrrev[j] = arr[i];
+  // }
+
+  // cout << "Reversed array: " << endl;
+  // for (int i=0; i<arrsize; i++) {
+  //   cout << " " << arrrev[i];
+  // }
+  // cout << endl;
+
+  auto p1 = &arr[0];
+  auto p2 = &arr[arrsize -1];
+  auto temp = 0;
+  while (p1 >= p2) {
+    temp = *p1;
+    *p1++ = *p2;
+    *p2-- = temp;
   }
 
   cout << "Reversed array: " << endl;
   for (int i=0; i<arrsize; i++) {
-    cout << " " << arrrev[i];
+    cout << " " << arr[i];
   }
   cout << endl;
 
