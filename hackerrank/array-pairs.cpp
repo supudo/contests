1f3a398f40e61b76c2b14762ad06205daec91756
Sergey Petrov <supudo@gmail.com>
2019-07-05 13:36:23 +0300

    rev

--- hackerrank/array-pairs.cpp
+++ hackerrank/array-pairs.cpp
@@ -44,7 +44,7 @@ int main() {
 }
 
 vector<string> split_string(string input_string) {
-  string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) { return x == y and x == ' '; });
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) { return x == y and x == ' '; });
   input_string.erase(new_end, input_string.end());
   while (input_string[input_string.length() - 1] == ' ') {
     input_string.pop_back();
